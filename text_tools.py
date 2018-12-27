204d9e9bd690335a921f90ec88b7879f32fc77ed
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2018-12-27 17:50:05 -0300

    Update text_tools.py

--- text_tools.py
+++ text_tools.py
@@ -2,16 +2,30 @@
 
 input_Text = None
 len_Text = None
+upper_Text = None
+keep_Working = 'Y'
+option = None
 
 # Defining functions
 
-def textLen(text):
+def textLen(text): # Function to count text lenght
     len_Text = len(text)
     print('Your text lenght is: ' + str(len_Text) + ' characters')
 
+def upperText(text): # Converting text to upper characters
+    upper_Text = text.upper()
+    print('Your text in upper characters is: ' + str(upper_Text))
+
 # Script logic
 
-print('Write something, please')
-input_Text = input() # Introducing some text
+while keep_Working == 'Y': # Menu Loop
 
-textLen(input_Text) # Counting the text lenght
+    print('What do you want to do?')
+    option = input()
+
+    
+
+    print('Write something, please')
+    input_Text = input() # Introducing some text
+
+    textLen(input_Text) # Counting the text lenght
