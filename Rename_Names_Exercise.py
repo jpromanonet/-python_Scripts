abf63a622941503c7c480dc812bd050f89bc6cbd
Juan P. Romano <programming@jpromano.net>
2019-03-05 20:10:38 -0300

    Rename files from american date to european date

---
+++ Rename_Names_Exercise.py
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+cd#! python3
+=======
+#! python3
+>>>>>>> 41d1d696daec11931efe29ea62f05f704c64b02f
+
+# Rename_Names_Exercise.py is the first exercise of chapter nine from the book 'Automating the boring stuff with python'
+
+# Importing libraries and frameworks
+
+import shutil
+import os
+import re
+
+# Declaring global variables
+
+datePattern = None
+amerFileName = None
+
+# User parameters
+
+# Program logic
+
+    # Create a regex that matches files with the American date format
+
+datePattern = re.compile(r(.*?) # all text before the date
+       ((0|1)?\d)-               # one or two digits for the month
+       ((0|1|2|3)?\d)-           # one or two digits for the day
+       ((19|20)\d\d)             # four digits for the year
+       (.*?)$                    # all text after the date
+          , re.VERBOSE)
+
+    # Loop over the files in the working directory
+
+for amerFileName in os.listdir('.'):
+    mo = datePattern.search(amerFileName)
+
+    # Skip files without a date
+
+    if mo == None:
+        continue
+
+    # Get the different parts of the filename
+
+    beforePart = mo.group(1)
+    monthPart  = mo.group(2)
+    dayPart    = mo.group(4)
+    yearPart   = mo.group(6)
+<<<<<<< HEAD
+    afterPart  = mo.group(8)
+
+    # Form the european style filename.
+
+    euroFileName = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
+=======
+    afterPart  = mo.group(8)
+>>>>>>> 41d1d696daec11931efe29ea62f05f704c64b02f
